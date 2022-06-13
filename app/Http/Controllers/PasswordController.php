13e98e57a25fdbbe76bc3be1fce5bcb7b47658d1
mukhan350192 <mukhan350192@gmail.com>
2022-06-13 15:00:49 +0600

    update

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -40,7 +40,7 @@ class PasswordController extends Controller
                 'updated_at' => Carbon::now(),
             ]);
 
-            $sms = "https://sms.i-credit.kz/api/resetPassword?phone=$phone&url='$url'";
+            $sms = "https://sms.i-credit.kz/api/resetPassword?phone=$phone&iin=$iin&id=$id";
             $smsResponse = file_get_contents($sms);
             $smsResponse = json_decode($smsResponse, true);
 
