20fd652c697fa6c5f0741d4324137c8cade88c0f
mukhan350192 <mukhan350192@gmail.com>
2022-07-18 15:50:51 +0600

    update

--- app/Http/Controllers/BioController.php
+++ app/Http/Controllers/BioController.php
@@ -80,7 +80,7 @@ class BioController extends Controller
                 'code' => $code,
             ]);
             $http = new Client(['verify' => false]);
-            $http->get('https://sms.i-credit.kz/api/typeOne',[
+            $http->get('http://37.18.30.37/api/typeOne',[
                 'query' => [
                     'code' => $code,
                     'phone' => $phone,
